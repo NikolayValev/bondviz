001d3b762471244fdf3e3fad20feae35fe2faeb4
Nikolay Valev <nikolaivalev@gmail.com>
2026-06-02 00:59:11 -0400

    feat(web): yield curve explorer (latest, shifts, spreads)

---
+++ web/app/yield-curve/page.tsx
@@ -0,0 +1,7 @@
+import { YieldCurveClient } from "./YieldCurveClient";
+
+export const metadata = { title: "Yield Curve · BondViz" };
+
+export default function YieldCurvePage() {
+  return <YieldCurveClient />;
+}
