0b86db0962c146a4cc7bfd9508ae0380c1f1ce4d
Nikolay Valev <nikolaivalev@gmail.com>
2026-06-22 13:44:48 -0400

    feat(web): inversion & recession signal page

---
+++ web/app/signal/page.tsx
@@ -0,0 +1,7 @@
+import { SignalClient } from "./SignalClient";
+
+export const metadata = { title: "Inversion & Recession Signal · BondViz" };
+
+export default function SignalPage() {
+  return <SignalClient />;
+}
